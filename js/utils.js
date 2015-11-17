ce9da66ba5a70961a5e57fdb5ce392d41bf1c111
Jesse MacFadyen <purplecabbage@gmail.com>
2015-11-17 10:33:28 -0800

    cleanup utils

--- js/utils.js
+++ js/utils.js
@@ -4,50 +4,25 @@ window.onerror = function (err, fn, ln) {
 };
 
 
- function debounce(func, delay, immediate) {
+function debounce(func, delay, immediate) {
+
     var timeout;
+
     return function() {
-        var context = this, args = arguments;
+        var context = this;
+        var args = arguments;
+
         function doLater() {
             timeout = null;
             func.apply(context, args);
         }
+
         var callNow = immediate && !timeout;
         clearTimeout(timeout);
         timeout = setTimeout(doLater, delay);
+        
         if (callNow) { 
             func.apply(context, args);
         }
     };
-};
-
-function alertIt(evt) {
-    alert(evt.type);
-
 }
-
-
-// var digits = (function(){
-//     var dig = {
-//         start:"mousedown",
-//         end:"mouseup",
-//         move:"mousemove",
-//         cancel:"touchcancel"
-//     }
-//     dig.canTouch = false;
-//     if(false){//('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-//     try  { 
-//         document.createEvent("TouchEvent"); 
-//         // update events to use touch events, they are available
-//         dig.canTouch = true; 
-//         dig.start = "touchstart";
-//         dig.end = "touchend";
-//         dig.move = "touchmove";
-//         dig.cancel = "touchcancel";
-//     } 
-//     catch (e) { //look for exception to feature-detection touch events.
-//         dig.canTouch = false;
-//     }
-//     }
-//     return dig;
-// })();
